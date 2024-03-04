2574b48b38fe3cb190df3f61fafa9fb92e49020c
yeri3177 <yeri3177@gmail.com>
2024-03-05 01:46:47 +0900

    섹션2-8.주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * 할인 금액이 잘 출력되는 것을 확인할 수 있다.
+ * 애플리케이션 로직으로 이렇게 테스트 하는 것은 좋은 방법이 아니다. JUnit 테스트를 사용하자. -> OrderServiceTest
+ */
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order); // order = Order{memberId=1, itemName='itemA', itemPrice=10000, discountPrice=1000}
+    }
+}
