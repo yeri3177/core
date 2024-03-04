f2a3ce3e8c7475ff267678d864ec6e4d9ae1e331
yeri3177 <yeri3177@gmail.com>
2024-03-05 00:14:08 +0900

    섹션2-5.회원 도메인 실행과 테스트

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+/**
+ * 순수 JAVA 코드만으로 테스트
+ * 애플리케이션 로직으로 이렇게 테스트 하는 것은 좋은 방법이 아니다.
+ * JUnit 테스트를 사용하자. -> MemberServiceTest
+ */
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new member = " + member.getName()); // new member = memberA
+        System.out.println("find Member = " + findMember.getName()); // find Member = memberA
+    }
+}
