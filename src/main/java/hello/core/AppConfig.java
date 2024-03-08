10f18449d9e2f766d60c85ea113957299f4655f6
yeri3177 <yeri3177@gmail.com>
2024-03-08 15:31:12 +0900

    섹션3-3.관심사의 분리

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,32 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ * 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
+ *
+ * AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+ * - MemberServiceImpl
+ * - MemoryMemberRepository
+ * - OrderServiceImpl
+ * - FixDiscountPolicy
+ *
+ * AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다.
+ * - MemberServiceImpl -> MemoryMemberRepository
+ * - OrderServiceImpl -> MemoryMemberRepository, FixDiscountPolicy
+ */
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,11 +13,18 @@ import hello.core.member.MemoryMemberRepository;
  */
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     /**
      * 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다.
      * FixDiscountPolicy -> RateDiscountPolicy
+     *
+     * FixDiscountPolicy 를 RateDiscountPolicy 로 변경하는 순간 OrderServiceImpl 의 소스 코드도 함께 변경해야 한다! -> OCP 위반
+     *
+     * 클라이언트 코드인 OrderServiceImpl 은 DiscountPolicy 의 인터페이스 뿐만 아니라 구체 클래스도 함께 의존한다.
+     * 그래서 구체 클래스를 변경할 때 클라이언트 코드도 함께 변경해야 한다.
+     * DIP 위반하므로 추상에만 의존하도록 변경(인터페이스에만 의존)
+     * DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존관계를 변경하면 된다.
      */
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
@@ -25,7 +32,18 @@ public class OrderServiceImpl implements OrderService {
      * 인터페이스에만 의존하도록 코드 변경
      * 실제 실행을 해보면 NPE(null pointer exception)가 발생
      */
-    private DiscountPolicy discountPolicy;
+//    private DiscountPolicy discountPolicy;
+
+    /**
+     * AppConfig 생성
+     */
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
